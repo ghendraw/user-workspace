22b37f60bbad7ce64dad51ce5d4298fcb48c9f98
Hendra <43094782+ghendraw@users.noreply.github.com>
2025-06-11 15:07:25 +0700

    Add resources/views/products/create.blade.php 1749629243119

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Create Product</h1>
+        <form action="{{ route('products.store') }}" method="POST">
+            @csrf
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" class="form-control" id="name" name="name" required>
+            </div>
+            <div class="form-group">
+                <label for="description">Description</label>
+                <textarea class="form-control" id="description" name="description"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="unit">Unit</label>
+                <input type="text" class="form-control" id="unit" name="unit" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Create</button>
+        </form>
+    </div>
+@endsection
